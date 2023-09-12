23ebcdc2c5664f480337d78101db9c321eea6b29
MalingaMp <malingace12@gmail.com>
2023-09-12 11:03:59 +0530

    commit

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-const panels=document.querySelectorAll('.panel')
-panels.forEach(panel =>{
-    panel.addEventListener('click',() =>{
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+const button = document.querySelectorAll(".ripple");
 
-function  removeActiveClasses(){
-    panels.forEach(panel =>{
-panel.classList.remove('active')
-    })
-}
+button.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    const buttonTop = e.target.offsetTop;
+    const buttonLeft = e.target.offsetLeft;
+
+    const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
+
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+    circle.style.top = yInside + "px";
+    circle.style.left = xInside + "px";
+
+    this.appendChild(circle);
+    setTimeout(() => circle.remove(), 500);
+  });
+});
